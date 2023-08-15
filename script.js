34050ad8688d09b4fbafede7f4dddaebb712e1dc
r480github <119364101+r480github@users.noreply.github.com>
2023-08-15 15:39:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openGame() {
+    var win = window.open()   
+    var url = "https://bing.com" 
+    var iframe = win.document.createElement('iframe')
+    iframe.style.width = "100%";
+    iframe.style.height = "100%"
+    iframe.style.border = "none";
+    iframe.src = url
+    win.document.body.appendChild(iframe)
+      
+  }
