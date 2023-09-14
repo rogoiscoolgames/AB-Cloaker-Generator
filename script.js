4f3007df91c84590bbd12c1f4550decbf049bf36
rogo! <119364101+r480github@users.noreply.github.com>
2023-09-14 18:13:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openGame() {
     var win = window.open()   
-    var url = "https://bing.com" 
+    var url = "https://scary-fawn-tank-top.cyclic.cloud/" 
     var iframe = win.document.createElement('iframe')
     iframe.style.width = "100%";
     iframe.style.height = "100%"
