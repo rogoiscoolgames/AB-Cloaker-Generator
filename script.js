6d30f3c7f940d6e3d17f5424bf494574661e111d
rogo! <119364101+r480github@users.noreply.github.com>
2024-10-25 20:23:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function openGame() {
-    var win = window.open()   
-    var url = "https://scary-fawn-tank-top.cyclic.cloud/" 
-    var iframe = win.document.createElement('iframe')
-    iframe.style.width = "100%";
-    iframe.style.height = "100%"
-    iframe.style.border = "none";
-    iframe.src = url
-    win.document.body.appendChild(iframe)
-      
-  }
+function opengame() {
+	var win = window.open()   
+	var url = "https://example.com" 
+	var iframe = win.document.createElement('iframe')
+	iframe.style.width = "100%";
+	iframe.style.height = "100%";
+	iframe.src = url;
+	win.document.body.style.margin = "0"; 
+	win.document.body.style.padding = "0"; 
+	win.document.body.appendChild(iframe);
+
+ }
